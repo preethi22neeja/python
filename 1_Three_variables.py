4be6311fe9ea7d6e0969055e096d1a370f4edea3
preethi22neeja <61535493+preethi22neeja@users.noreply.github.com>
2020-12-02 21:56:57 -0800

    Add files via upload
    Task 2

---
+++ 1_Three_variables.py
@@ -0,0 +1,15 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = int(input())
+30
+>>> b = int(input())
+50
+>>> c = int(input())
+20
+>>> print(a/10)
+3.0
+>>> print (b*20)
+1000
+>>> print(c + 60)
+80
+>>> 
